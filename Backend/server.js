30c085f616ab92fb589fee9ac589736da1cefa59
RazrtroN <tiwari.devansh2002@gmail.com>
2026-03-29 18:09:25 +0530

    cleaned project structure and removed duplicates

--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");   // 👈 MOVE HERE
 
 dotenv.config();
 
 const app = express();
-
-app.use(cors());                // 👈 MOVE HERE (TOP)
 app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
-});
-
-// Routes
-app.use("/api/v1/auth", require("./routes/authRoutes"));
-app.use("/api/v1/tasks", require("./routes/taskRoutes"));
-
-const { protect } = require("./middleware/authMiddleware");
-
-app.get("/api/v1/protected", protect, (req, res) => {
-  res.json({
-    message: "You accessed protected route!",
-    user: req.user
-  });
 });
 
 // MongoDB connection
@@ -36,3 +20,16 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+
+const { protect } = require("./middleware/authMiddleware");
+
+app.get("/api/v1/protected", protect, (req, res) => {
+  res.json({
+    message: "You accessed protected route!",
+    user: req.user
+  });
+});
+
+app.use("/api/v1/tasks", require("./routes/taskRoutes"));
